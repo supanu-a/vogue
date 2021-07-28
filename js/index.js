effc6d05d6eee20c1d22596cbbf76ca6ec13524e
supanu-a <84338299+supanu-a@users.noreply.github.com>
2021-07-28 17:38:57 +0900

    v1.0

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$('.btnMenu').on('click', function(){
+  $(this).fadeOut()
+  $('nav').addClass('on')
+  $('section').addClass('on')
+})
+$('#gnb li a').on('click', function(){
+  $('nav').removeClass('on')
+  $('.btnMenu').fadeIn(800)
+  $('section').removeClass('on')
+  let num = $(this).parent().index()
+  $('section > div').eq(num).siblings().removeClass('on')
+  $('section > div').eq(num).addClass('on')
+})
+
